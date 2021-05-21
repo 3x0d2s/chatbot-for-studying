c3afad104974fa50d3f010e929d99426d9639d71
3x0d2s-MSI <exodus.yarwood@icloud.com>
2021-05-21 22:58:34 +0300

    Прописал док-строки для функций.

--- scripts/check_InputData.py
+++ scripts/check_InputData.py
@@ -1,7 +1,8 @@
 import datetime
 
 
-def Check_Date(date):  # Проверка даты на корректность
+def Check_Date(date):
+    """Проверяет дату на корректность."""
     try:
         datetime.datetime.strptime(date, '%d.%m.%Y')
         return True
@@ -10,13 +11,15 @@ def Check_Date(date):  # Проверка даты на корректность
 
 
 def Check_Lesson(lesson):  # Проверка имени урока на корректность
+    """Проверяет имя урока на длину."""
     if len(lesson) <= 32:
         return True
     else:
         return False
 
 
-def Check_Tasks(task):  # Проверка задания на корректность
+def Check_Tasks(task):
+    """Проверяет задание на длину."""
     if len(task) <= 512:
         return True
     else:
