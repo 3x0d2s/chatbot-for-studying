b2938d62787a1a9e542b1395add8163b4b250b88
3x0d2s-msi <exodus.yarwood@icloud.com>
2022-10-24 22:00:04 +0300

    2022-10-24 Dev: исправлены пути до дампа, бд, и файла настроек.

--- bot/config/initial_config.py
+++ bot/config/initial_config.py
@@ -3,7 +3,7 @@ import os
 #TOKEN = "ENTER YOUR API_TOKEN"
 #GROUP_ID = 'ENTER YOUR GROUP_ID'
 #
-PATH_DB = f'db{os.sep}db.db'
-PATH_DUMP = f'db{os.sep}db_dump.sql'
-PATH_SETTINGS = f'{os.sep}config{os.sep}Settings.ini'
+PATH_DB = f'bot{os.sep}db{os.sep}db.db'
+PATH_DUMP = f'bot{os.sep}db{os.sep}db_dump.sql'
+PATH_SETTINGS = f'bot{os.sep}config{os.sep}Settings.ini'
 #
